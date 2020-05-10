d382b3512cb59838405329da6af1d731a6aed5cc
cahaug <caleahaug@gmail.com>
2020-05-10 15:36:23 -0700

    day 1 and 2 complete, multiplying and loading successfully

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,36 +7,70 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # initialize RAM of 256
+        self.ram = [0] * 256
+        # program counter
+        self.pc = 0
+        # 8-bit registry
+        self.reg = [0] * 8
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
+        return 0b00000001 #true
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        # if there is a system variable
+        if len(sys.argv) > 1:
+            # load the file at the address
+            program_file = sys.argv[1]
+            # read file 
+            program = open(program_file, "r")
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+            # for each instruction
+            for line in program:
+                # remove extra stuff
+                line = line.split('#',1)[0].strip()
+                # add the line to ram
+                self.ram[address] = int(f'0b{line}', 2)
+                # increment address variable to run next loop
+                address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # otherwise, demo the default program
+        else:
+            program = [
+                # From print8.ls8
+                0b10000010, # LDI R0,8
+                0b00000000,
+                0b00001000,
+                0b01000111, # PRN R0
+                0b00000000,
+                0b00000001, # HLT
+            ]
+
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        #implement math functions
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] = self.reg[reg_a] / self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +96,39 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # start the program
+        isRunning = True
+
+        # instruction codes
+        ldi = 0b10000010
+        prn = 0b01000111
+        hlt = 0b00000001
+        mul = 0b10100010
+
+        while isRunning:
+            # load next instruction into instructions register
+            ir = self.ram_read(self.pc)
+            # cache next two instructions
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # if LDI Instruction: save the value into the register
+            if ir == ldi:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            # if PRN Instruction: print the value from register
+            elif ir == prn:
+                print(self.reg[operand_a])
+                self.pc += 2
+            # if MUL Instruction: get the product of the two register values specified, save in the first
+            elif ir == mul:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+            # if HLT Instruction: halt the program
+            elif ir == hlt:
+                running = False
+                self.pc += 1
+            # if unrecognized command
+            else:
+                print(f'Unknown instruction register: {ir}')
+                # system crash
+                sys.exit(1)
